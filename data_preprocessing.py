709873f78f84996979f5145799da79526c6b476d
hatrang22 <hatrangphung@gmail.com>
2022-12-23 16:55:46 +0100

    fix default extract name and read fasta (chevrons)

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,8 +1,9 @@
 import  gzip
 import os
 import tarfile
+import warnings
 
-def read_phylum(fasta_path, gff_path, extract_name="ncbi-genomes-2022-12-13"):
+def read_phylum(fasta_path, gff_path):
     """
     Parameters
     ----------
@@ -11,9 +12,6 @@ def read_phylum(fasta_path, gff_path, extract_name="ncbi-genomes-2022-12-13"):
 
     gff_path: str
             Path to gff tar file.
-
-    extract_name: str, default ncbi-genomes-2022-12-13
-            Default directory name when extracting.
 
     Returns
     -------
@@ -29,12 +27,12 @@ def read_phylum(fasta_path, gff_path, extract_name="ncbi-genomes-2022-12-13"):
     fasta_dir = os.path.splitext(fasta_path)[0] # remove extension file
     with tarfile.open(fasta_path) as f:
         f.extractall(fasta_dir)
-    fasta_dir = os.path.join(fasta_dir, extract_name) # files extracted in fasta_dir
+    fasta_dir = os.path.join(fasta_dir, os.listdir(fasta_dir)[0]) # files extracted in fasta_dir
 
     gff_dir = os.path.splitext(gff_path)[0] # remove extension file
     with tarfile.open(gff_path) as f:
         f.extractall(gff_dir)
-    gff_dir = os.path.join(gff_dir, extract_name) # files extracted in gffr_dir
+    gff_dir = os.path.join(gff_dir, os.listdir(gff_dir)[0]) # files extracted in gffr_dir
 
     list_fasta = [f for f in os.listdir(fasta_dir) if f.endswith(".gz")]
     list_fasta.sort()  # list of sorted fasta names
@@ -53,27 +51,35 @@ def read_phylum(fasta_path, gff_path, extract_name="ncbi-genomes-2022-12-13"):
         gan = "_".join(gan[:2])  # get accession number for gff
 
         if fan == gan:  # check if accession numbers are consistent
-            ret["accession_number"].append(fan)
 
-        else:
-            raise ValueError(f"Inconsistent accession number between fasta ({fan}) and gff ({gan})")
-
-        with gzip.open(os.path.join(fasta_dir, fasta_fn), "rt") as gzip_fasta:
-
-            fasta = [f for f in gzip_fasta.readlines() if f[0].isalpha()] # Conserver uniquement les lignes commencant par une lettre
-            fasta = "".join(fasta) # concatener list de string en str
-            fasta = fasta.replace("\n","") # replace \n
+            fasta, gff = preprocessing_fasta_gff(os.path.join(fasta_dir, fasta_fn), os.path.join(gff_dir, gff_fn))
 
             ret["fasta"].append(fasta)
 
-        with gzip.open(os.path.join(gff_dir, gff_fn), "rt") as gzip_gff:
-
-            gff = [f.split("\t") for f in gzip_gff.readlines() if f[0].isalpha()] # Conserver uniquement les lignes commencant par une lettre
-
             ret["gff"].append(gff)
+
+            ret["accession_number"].append(fan)
+
+        else:
+            warnings.warn(f"Inconsistent accession number between fasta file ({fan}) and gff file ({gan})")
             
     return ret
 
-def preprocessing_fasta_gff():
+def preprocessing_fasta_gff(fasta_gz, gff_gz):
 
-    return
+    with gzip.open(fasta_gz, "rt") as gzip_fasta:
+
+        fasta= []
+        for f in gzip_fasta.readlines():
+            if f[0].isalpha():
+                fasta.append(f)
+            elif fasta:
+                break
+        fasta = "".join(fasta) # concatener list de string en str
+        fasta = fasta.replace("\n","") # replace \n
+
+    with gzip.open(gff_gz, "rt") as gzip_gff:
+
+        gff = [f.split("\t") for f in gzip_gff.readlines() if f[0].isalpha()] # Conserver uniquement les lignes commencant par une lettre
+
+    return fasta, gff
