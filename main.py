5da2ddf194543e4dec29cc0c7650bdd05e45b5b4
Ha Trang Phung <86566252+hatrang22@users.noreply.github.com>
2023-01-19 16:15:18 +0100

    Update main.py

--- main.py
+++ main.py
@@ -85,4 +85,4 @@ for method in LIST_CLASSIFICATION_METHOD:
     mat, global_score = classify(dfs_start, method, top3_only=False)
     
     plot_classify(mat, method, LIST_PHYLUM, "start")
-    print(f"Global score: {global_score}")
+    print(f"    Global score: {global_score}")
