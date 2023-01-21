26366e871f8c9af8c80c90b672f62ebbf9ab0ab5
toccoc <90920855+toccoc@users.noreply.github.com>
2023-01-21 17:36:10 +0100

    Update ml_tools.py

--- ml_tools.py
+++ ml_tools.py
@@ -9,6 +9,7 @@ from sklearn.svm import SVC
 from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
+from stats_and_graphs import df_pour_PCA
 
 def clustering(dfs, n_cluster, method, top3_only=False):
     
