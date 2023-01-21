7819d7c31d908d6b1b71dbdd0c27ec15074a525f
toccoc <90920855+toccoc@users.noreply.github.com>
2023-01-21 16:59:32 +0100

    Update ml_tools.py

--- ml_tools.py
+++ ml_tools.py
@@ -93,3 +93,80 @@ def classify(dfs, method, test_size=0.33, split_seed=0, training_seed=0, top3_on
     global_score = accuracy_score(y_test, y_pred)
 
     return mat, global_score
+
+#%% ROC curve
+from sklearn.metrics import roc_auc_score
+import matplotlib.pyplot as plt
+
+def calculate_tpr_fpr(y_real, y_pred):
+    '''
+    Calculates the True Positive Rate (tpr) and the True Negative Rate (fpr) based on real and predicted observations
+    
+    Args:
+        y_real: The list or series with the real classes
+        y_pred: The list or series with the predicted classes
+        
+    Returns:
+        tpr: The True Positive Rate of the classifier
+        fpr: The False Positive Rate of the classifier
+    '''
+    
+    # Calculates the confusion matrix and recover each element
+    cm = confusion_matrix(y_real, y_pred)
+    TN = cm[0, 0]
+    FP = cm[0, 1]
+    FN = cm[1, 0]
+    TP = cm[1, 1]
+    
+    # Calculates tpr and fpr
+    tpr =  TP/(TP + FN) # sensitivity - true positive rate
+    fpr = 1 - TN/(TN+FP) # 1-specificity - false positive rate
+    
+    return tpr, fpr
+
+def get_all_roc_coordinates(y_real, y_proba):
+    '''
+    Calculates all the ROC Curve coordinates (tpr and fpr) by considering each point as a threshold for the predicion of the class.
+    
+    Args:
+        y_real: The list or series with the real classes.
+        y_proba: The array with the probabilities for each class, obtained by using the `.predict_proba()` method.
+        
+    Returns:
+        tpr_list: The list of TPRs representing each threshold.
+        fpr_list: The list of FPRs representing each threshold.
+    '''
+    tpr_list = [0]
+    fpr_list = [0]
+    for i in range(len(y_proba)):
+        threshold = y_proba[i]
+        y_pred = y_proba >= threshold
+        tpr, fpr = calculate_tpr_fpr(y_real, y_pred)
+        tpr_list.append(tpr)
+        fpr_list.append(fpr)
+    return tpr_list, fpr_list
+
+
+def plot_roc_curve(tpr, fpr, classes, scatter = False):
+    '''
+    Plots the ROC Curve by using the list of coordinates (tpr and fpr).
+    
+    Args:
+        tpr: The list of TPRs representing each coordinate.
+        fpr: The list of FPRs representing each coordinate.
+        scatter: When True, the points used on the calculation will be plotted with the line (default = True).
+    '''
+    if scatter:
+        plt.scatter(x = fpr, y = tpr)
+    
+    fpr_tpr = pd.DataFrame({'fpr': fpr, 'tpr': tpr})
+    fpr_tpr.sort_values('fpr', inplace=True)
+
+    plt.plot(fpr_tpr['fpr'], fpr_tpr['tpr'])
+    plt.plot([0,1],[0,1],'r') #diagonal line
+    plt.xlim(-0.05, 1.05)
+    plt.ylim(-0.05, 1.05)
+    plt.xlabel("False Positive Rate")
+    plt.ylabel("True Positive Rate")
+    plt.title('ROC curve of '+ classes)
+    plt.show()
